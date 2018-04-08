53311d600cbee72e748f3764d6de73d52e17f5f3
Clément NONN <clement.nonn@gmail.com>
2018-04-08 15:52:47 +0200

    Initial Commit

---
+++ MappingKit/MappingKit.h
@@ -0,0 +1,19 @@
+//
+//  MappingKit.h
+//  MappingKit
+//
+//  Created by Clément NONN on 08/04/2018.
+//  Copyright © 2018 Clément NONN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MappingKit.
+FOUNDATION_EXPORT double MappingKitVersionNumber;
+
+//! Project version string for MappingKit.
+FOUNDATION_EXPORT const unsigned char MappingKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MappingKit/PublicHeader.h>
+
+
